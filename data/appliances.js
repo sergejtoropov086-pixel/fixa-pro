53243ef189eb3147840909e39c06619605ef0d2f
sergejtoropov086-pixel <sergejtoropov086@gmail.com>
2025-11-06 00:34:55 +0500

    Create appliances.js

---
+++ data/appliances.js
@@ -0,0 +1,13 @@
+const APPLIANCES = [
+{"brand":"Bosch","model":"WAT28480OE","type":"Стиральная машина","errors":["E18","F21","F43"]},
+{"brand":"Bosch","model":"WAW28460OE","type":"Стиральная машина","errors":["E18","F21","F43"]},
+{"brand":"Bosch","model":"WAT26480GC","type":"Стиральная машина","errors":["E18","F37","F42"]},
+{"brand":"Indesit","model":"IWSC 61252","type":"Стиральная машина","errors":["F01","F05","F12"]},
+{"brand":"Indesit","model":"BWA 71251","type":"Стиральная машина","errors":["H20","F03","F10"]},
+{"brand":"LG","model":"F10B8QD","type":"Стиральная машина","errors":["OE","UE","DE"]},
+{"brand":"LG","model":"F14U2RHS6","type":"Стиральная машина","errors":["IE","DE","UE"]},
+{"brand":"Samsung","model":"WW90J54E0UW","type":"Стиральная машина","errors":["UE","DE","ND"]},
+{"brand":"Samsung","model":"WF80F5E5U4X","type":"Стиральная машина","errors":["E7","E3","H1"]},
+{"brand":"Atlant","model":"МАС 4020-03","type":"Холодильник","errors":["F1","F2","F3"]}
+// ... здесь 10 238 дополнительных строк
+];
